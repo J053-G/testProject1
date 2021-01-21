83d2ef35e19a9c07ba4ca169516d0f5210bab13b
P.M. Student <p.m.student@RS-F251-M12.local>
2021-01-21 14:10:34 -0600

    Added a button

---
+++ testProject1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  testProject1
+//
+//  Created by P.M. Student on 1/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Button")/*@END_MENU_TOKEN@*/
+            }
+            Text("Hello, world!")
+                .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testProject1/testProject1App.swift
@@ -0,0 +1,17 @@
+//
+//  testProject1App.swift
+//  testProject1
+//
+//  Created by P.M. Student on 1/21/21.
+//
+
+import SwiftUI
+
+@main
+struct testProject1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
